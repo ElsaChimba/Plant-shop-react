b2446e54f5857ea95c6001296c7d0cc6df839624
ELsaChimba <Elsachimba28@gmail.com>
2025-08-17 21:36:13 +0100

    resolvendo a page de flores

--- services/plantsApi.ts
+++ services/plantsApi.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const res = await fetch(
-    "https://perenual.com/api/v2/species-list?key=sk-7Kzw68a20821b7dce11880&q=flower"
+    "https://trefle.io/api/v1/flower?token=n6ndIJXVarXHcn0RuTIigwI_Z5xTY1d2iiyML5cKoR8=flower"
   );
   const data = await res.json();
   return NextResponse.json(Array.isArray(data?.data) ? data.data : []);
@@ -10,7 +10,7 @@ export async function GET() {
 
 export async function fetchPlantas() {
   const res = await fetch(
-    "https://perenual.com/api/v2/species-list?key=sk-7Kzw68a20821b7dce11880&q=plant"
+    "https://trefle.io/api/v1/plants?token=n6ndIJXVarXHcn0RuTIigwI_Z5xTY1d2iiyML5cKoR8=plant"
   );
   const data = await res.json();
   return NextResponse.json(Array.isArray(data?.data) ? data.data : []);
