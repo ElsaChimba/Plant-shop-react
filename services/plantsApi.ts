92a26db3418184fae4bb70decfa3f584d43424ef
ELsaChimba <Elsachimba28@gmail.com>
2025-08-17 21:44:04 +0100

    mexendo na api de plantas

--- services/plantsApi.ts
+++ services/plantsApi.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const res = await fetch(
-    "https://trefle.io/api/v1/flower?token=n6ndIJXVarXHcn0RuTIigwI_Z5xTY1d2iiyML5cKoR8"
+    "https://trefle.io/api/v1/plants?token=n6ndIJXVarXHcn0RuTIigwI_Z5xTY1d2iiyML5cKoR8"
   );
   const data = await res.json();
   return NextResponse.json(Array.isArray(data?.data) ? data.data : []);
