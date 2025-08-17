7004b8885b567226d5b1ad3b2c4e1773fdb2d97a
ELsaChimba <Elsachimba28@gmail.com>
2025-08-17 17:38:06 +0100

    corrigindo bug no vercel

---
+++ app/flores/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+
+export default function FloresPage() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Flores</h1>
+            <Footer />
+        </div>
+    );
+}
